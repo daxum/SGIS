ca379dbeb64ad026867b47d070ec798bf0a7e026
daxum <daxum144@gmail.com>
2018-03-17 14:41:01 -0500

    Use new camera

--- src/Game.cpp
+++ src/Game.cpp
@@ -72,8 +72,11 @@ void Game::loadScreens(DisplayEngine& display) {
 	mainMenu->addObject(ground);
 	mainMenu->addObject(square);
 
+	//Set camera
+	mainMenu->getCamera().setTarget(square);
+
 	//Hack for testing
-	mainMenu->getRenderData().camera.move(0.0f, 200.0f, 0.01f);
+	mainMenu->getCamera().move(0.0f, 200.0f, 0.01f);
 
 	display.pushScreen(mainMenu);
 }
